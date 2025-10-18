2bcc6a16e7c22013520bec1e0daf502f2cbfe2ed
kleinyumoretti <1003632472@qq.com>
2025-10-18 18:38:41 +0800

    实现token校验功能，实现过滤器拦截

--- focus-pojo/src/main/java/com/klein/entity/User.java
+++ focus-pojo/src/main/java/com/klein/entity/User.java
@@ -1,11 +1,14 @@
 package com.klein.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class User implements Serializable {
+    private Integer id;
     private String username;
     private String password;
+    private String token;
 }
