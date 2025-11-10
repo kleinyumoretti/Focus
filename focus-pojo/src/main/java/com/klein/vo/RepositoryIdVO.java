c076fca3dd82637bd651632719db571b39d39a27
kleinyumoretti <1003632472@qq.com>
2025-11-10 22:17:10 +0800

    重修

--- focus-pojo/src/main/java/com/klein/vo/RepositoryIdVO.java
+++ focus-pojo/src/main/java/com/klein/vo/RepositoryIdVO.java
@@ -5,6 +5,6 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class RepositoryVO implements Serializable {
+public class RepositoryIdVO implements Serializable {
     Integer repositoryId;
 }
